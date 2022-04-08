f1c1eda9676a015eb74b981878666bc0fb248f3b
iamgauraav <iamgauraav@gmail.com>
2022-04-08 21:49:23 +0530

    Added UC4 : Validating Mobile Number in user registration

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -7,13 +7,14 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Email ID");
+            Console.WriteLine("Enter Phone Number");
             string inStr = Console.ReadLine();
             string firsName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]";
+            string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";
 
-            Regex regex = new Regex(emailId);
+            Regex regex = new Regex(phoneNum);
             Console.WriteLine(regex.IsMatch(inStr));
         }
     }
