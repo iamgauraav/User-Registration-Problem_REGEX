9f114b71622ad7f4aa9a7a9d0d7662973a66defa
iamgauraav <iamgauraav@gmail.com>
2022-04-08 22:23:38 +0530

    Added UC6 : Validating Password in user registration

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -13,7 +13,7 @@ namespace UserRegistration
             string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]";
             string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";
-            string password = @"[a-zA-Z]{8}$";
+            string password = @"[A-Z]{1}[a-zA-Z]{7}$";
 
             Regex regex = new Regex(password);
             Console.WriteLine(regex.IsMatch(inStr));
