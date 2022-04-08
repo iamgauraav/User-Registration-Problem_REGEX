7b67f901e2deed90d0195f4c3ebef4806c525fe6
iamgauraav <iamgauraav@gmail.com>
2022-04-08 21:36:59 +0530

    Added UC3 : To Validating Email in user registration

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -7,12 +7,13 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter last name");
+            Console.WriteLine("Enter Email ID");
             string inStr = Console.ReadLine();
-
+            string firsName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
+            string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]";
 
-            Regex regex = new Regex(lastName);
+            Regex regex = new Regex(emailId);
             Console.WriteLine(regex.IsMatch(inStr));
         }
     }
