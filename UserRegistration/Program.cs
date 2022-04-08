976fd5d607046323848c3518948d9553382bfe84
iamgauraav <iamgauraav@gmail.com>
2022-04-08 21:57:02 +0530

    Added UC5 : Validating Password in user registration

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -13,8 +13,9 @@ namespace UserRegistration
             string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]";
             string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";
+            string password = @"[a-zA-Z]{8}$";
 
-            Regex regex = new Regex(phoneNum);
+            Regex regex = new Regex(password);
             Console.WriteLine(regex.IsMatch(inStr));
         }
     }
